a19dd74a373247042a1efb361e169fe3ee852c9e
vashakidzenino <nino.vashakidze14b@geolab.edu.ge>
2020-04-28 04:09:21 +0400

    +

--- styles/waixmese.js
+++ styles/waixmese.js
@@ -1,17 +1,36 @@
-let makeOrder = document.getElementsByClassName('button');
+let stausChange = document.querySelectorAll('.disappear');
+let burgerSelect = document.querySelector('.burgerNav');
+burgerSelect.classList.add('hidden');
 
-let bl = makeOrder.length;
 
-for(let i = 0; i < bl; i++){
-	
-	makeOrder[i].addEventListener('click', function(){
-		this.classList.toggle('added');
-		if (this.classList.contains('added')){
-			this.textContent = 'დამატებულია';
-		} else {
-			this.textContent = 'დამატება';
-		}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('burger-Icon').onclick = toggleMenu;
+
+    // თოგლ მენიუ ბურგერის ქლიქზე.
+
+    function toggleMenu () {
+        for (let item of stausChange) {
+            item.classList.toggle('hidden');
+            burgerSelect.classList.toggle('hidden');
+        }
+    }
+
+
+	let makeOrder = document.getElementsByClassName('button');
+
+	let bl = makeOrder.length;
+
+	for(let i = 0; i < bl; i++){
 		
-	})
-	
-}
+		makeOrder[i].addEventListener('click', function(){
+			this.classList.toggle('added');
+			if (this.classList.contains('added')){
+				this.textContent = 'დამატებულია';
+			} else {
+				this.textContent = 'დამატება';
+			}
+			
+		})
+		
+	}
+});
